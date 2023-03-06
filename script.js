d7ed7f895909b5435abe27ee7f687ed0b77ad280
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-03-06 16:10:05 +0800

    changes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // setting dom variables
 const pad = document.querySelector(".container");
 const startbtn = document.createElement("button");
-startbtn.addEventListener("click", makeGrid());
-pad.appendChild(startbtn);
+
+
 
 // to make squares inside the grid
 function makeGrid(number) {
-    for (let i = 0; i < 30; i ++) {
+    for (let i = 0; i < 256; i ++) {
         let square = document.createElement("div");
         pad.appendChild(square);
     }

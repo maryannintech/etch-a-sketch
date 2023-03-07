041e8ab2ff74b51db8048d73028b01d0e1131b0e
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-03-07 12:30:59 +0800

    changes

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 // setting dom variables
 const pad = document.querySelector(".container");
 const startbtn = document.createElement("button");
-const xsmallbtn = document.querySelector(".xsmall");
+
+// buttons
+const xsmallbtn = document.querySelector(".xsmall").addEventListener("click", function(){makeGrid(3);});
 const smallbtn = document.querySelector(".small");
 const mediumbtn = document.querySelector(".medium");
 const largebtn = document.querySelector(".large");
 const xlargebtn = document.querySelector(".xlarge");
 
 
+
+
 // to make squares inside the grid
 function makeGrid(number) {
-    for (let i = 0; i < 256; i ++) {
+    let size = number*number;
+    for (let i = 0; i < size; i ++) {
         let square = document.createElement("div");
-        square.style.cssText = "border: 2px black solid;"
         pad.appendChild(square);
+        square.style.cssText = "border: 1px solid black; border-radius: 10px;"
     }
 }
 

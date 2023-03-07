be3318907f1205929f419db8af8e0dc2cc8c6392
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-03-07 21:33:57 +0800

    added the toptext class and visibility function

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 // setting dom variables
 const pad = document.querySelector(".container");
-const startbtn = document.createElement("button");
-
-// size buttons
-const tinybtn = document.querySelector(".tiny").addEventListener("click", () => {makeGrid(3)});
-const smallbtn = document.querySelector(".small").addEventListener("click", () => {makeGrid(6)});
-const mediumbtn = document.querySelector(".medium").addEventListener("click", () => {makeGrid(9)});
-const largebtn = document.querySelector(".large").addEventListener("click", () => {makeGrid(12)});
-const giantbtn = document.querySelector(".giant").addEventListener("click", () => {makeGrid(15)});
 
 // tools buttons
 const rainbowbtn = document.querySelector(".randommode");
@@ -15,17 +7,28 @@ const drawbtn = document.querySelector(".drawbtn");
 const eraserbtn = document.querySelector(".eraser");
 const resetbtn = document.querySelector(".reset").addEventListener("click", resetProgram);
 
+// size buttons
+const smallbtn = document.querySelector(".small").addEventListener("click", () => {chooseGrid(6)});
+const mediumbtn = document.querySelector(".medium").addEventListener("click", () => {chooseGrid(9)});
+const largebtn = document.querySelector(".large").addEventListener("click", () => {chooseGrid(12)});
+
 // to make divs inside the container
-function makeGrid(num) {
-    let size = num **2;
+function chooseGrid(num) {
+    toptexthide = document.querySelector(".toptext");
+    sizesbtns = document.querySelector(".sizes");
+    const size = num **2;
     pad.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     pad.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+
     for (let i = 0; i < size; i ++) {
-        let square = document.createElement("div");
+        const square = document.createElement("div");
         square.style.cssText = "border: 1px solid grey; border-radius: 3px;"
         pad.appendChild(square).className = "grid-items";
+        toptexthide.style.visibility = "hidden";
+        sizesbtns.style.visibility = "hidden";
     }
 
+    // selecting each square item
     const squareItems = pad.querySelectorAll(".grid-items");
     squareItems.forEach((item) => {
         item.addEventListener("mouseover", () => {
@@ -38,7 +41,6 @@ function makeGrid(num) {
 function chooseRandomColor() {
     let pallete = ['#386641', '#6A994E', '#A7C957', '#F2E8CF', '#BC4749'];
     let randomcolorChoice = pallete[Math.floor(Math.random()*pallete.length)];
-
 }
 
 // to let user draw 
@@ -53,3 +55,4 @@ function erase() {
 function resetProgram() {
     window.location.reload();
 }
+

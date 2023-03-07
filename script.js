0efb10f55e24491931796013a3e16be9838b3e12
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-03-07 15:56:54 +0800

    update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const pad = document.querySelector(".container");
 const startbtn = document.createElement("button");
 
 // size buttons
-const tinybtn = document.querySelector(".tiny").addEventListener("click", function(){makeGrid(3,3);});
-const smallbtn = document.querySelector(".small").addEventListener("click", function(){makeGrid(6,6);});
-const mediumbtn = document.querySelector(".medium").addEventListener("click", function(){makeGrid(9,9);});
-const largebtn = document.querySelector(".large").addEventListener("click", function(){makeGrid(12,12);});
-const giantbtn = document.querySelector(".giant").addEventListener("click", function(){makeGrid(15,15);});
+const tinybtn = document.querySelector(".tiny").addEventListener("click", function(){makeGrid(3);});
+const smallbtn = document.querySelector(".small").addEventListener("click", function(){makeGrid(6);});
+const mediumbtn = document.querySelector(".medium").addEventListener("click", function(){makeGrid(9);});
+const largebtn = document.querySelector(".large").addEventListener("click", function(){makeGrid(12);});
+const giantbtn = document.querySelector(".giant").addEventListener("click", function(){makeGrid(15);});
 
 // tools buttons
 const rainbowbtn = document.querySelector(".rainbowmode");
@@ -16,8 +16,8 @@ const eraserbtn = document.querySelector(".eraser");
 const resetbtn = document.querySelector(".reset").addEventListener("click", resetProgram);
 
 // to make squares inside the grid
-function makeGrid(rows, cols) {
-    let size = rows*cols;
+function makeGrid(num) {
+    let size = num **2;
     for (let i = 0; i < size; i ++) {
         let square = document.createElement("div");
         square.style.cssText = "border: 1px solid grey; border-radius: 10px;"
@@ -25,9 +25,9 @@ function makeGrid(rows, cols) {
     }
 }
 
-// to start drawing 
-function draw() {
-    document.querySelector(".grid-items").style.color="black";
+// user select  
+function rainbowMode() {
+
 }
 
 function resetProgram() {

e99600b0b9acfcbd1b385740360cbbed1bf04be3
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-03-07 21:04:31 +0800

    added grid template columns and rows

--- script.js
+++ script.js
@@ -18,17 +18,27 @@ const resetbtn = document.querySelector(".reset").addEventListener("click", rese
 // to make divs inside the container
 function makeGrid(num) {
     let size = num **2;
+    pad.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    pad.style.gridTemplateRows = `repeat(${num}, 1fr)`;
     for (let i = 0; i < size; i ++) {
         let square = document.createElement("div");
-        square.style.cssText = "border: 1px solid grey; border-radius: 10px;"
+        square.style.cssText = "border: 1px solid grey; border-radius: 3px;"
         pad.appendChild(square).className = "grid-items";
     }
+
+    const squareItems = pad.querySelectorAll(".grid-items");
+    squareItems.forEach((item) => {
+        item.addEventListener("mouseover", () => {
+            item.style.backgroundColor = "pink";
+        })
+    })
 }
 
 // generate random color by given pallete
 function chooseRandomColor() {
     let pallete = ['#386641', '#6A994E', '#A7C957', '#F2E8CF', '#BC4749'];
     let randomcolorChoice = pallete[Math.floor(Math.random()*pallete.length)];
+
 }
 
 // to let user draw 

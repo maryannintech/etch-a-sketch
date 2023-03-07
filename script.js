3d831b6ec0bcd67845865a3f3b61429661dd883d
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-03-07 22:06:19 +0800

    reduced some global variables in script.js

--- script.js
+++ script.js
@@ -1,37 +1,25 @@
-// setting dom variables
-const pad = document.querySelector(".container");
-
-// tools buttons
-const rainbowbtn = document.querySelector(".randommode");
-const drawbtn = document.querySelector(".drawbtn");
-const eraserbtn = document.querySelector(".eraser");
-const resetbtn = document.querySelector(".reset").addEventListener("click", resetProgram);
-
-// size buttons
-const smallbtn = document.querySelector(".small").addEventListener("click", () => {chooseGrid(6)});
-const mediumbtn = document.querySelector(".medium").addEventListener("click", () => {chooseGrid(9)});
-const largebtn = document.querySelector(".large").addEventListener("click", () => {chooseGrid(12)});
-
 // to make divs inside the container
-function chooseGrid(num) {
+function makeGrid(num) {
+    const pad = document.querySelector(".container");
     toptexthide = document.querySelector(".toptext");
     sizesbtns = document.querySelector(".sizes");
+
     const size = num **2;
     pad.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     pad.style.gridTemplateRows = `repeat(${num}, 1fr)`;
-
     for (let i = 0; i < size; i ++) {
         const square = document.createElement("div");
-        square.style.cssText = "border: 1px solid grey; border-radius: 3px;"
+        square.style.cssText = "border: 1px solid grey; border-radius: 5px;"
         pad.appendChild(square).className = "grid-items";
-        toptexthide.style.visibility = "hidden";
-        sizesbtns.style.visibility = "hidden";
     }
+
+    toptexthide.style.visibility = "hidden";
+    sizesbtns.style.visibility = "hidden";
 
     // selecting each square item
     const squareItems = pad.querySelectorAll(".grid-items");
     squareItems.forEach((item) => {
-        item.addEventListener("mouseover", () => {
+        item.addEventListener("mousemove", () => {
             item.style.backgroundColor = "pink";
         })
     })
@@ -56,3 +44,13 @@ function resetProgram() {
     window.location.reload();
 }
 
+// tools buttons
+const rainbowbtn = document.querySelector(".randommode");
+const drawbtn = document.querySelector(".drawbtn");
+const eraserbtn = document.querySelector(".eraser");
+const resetbtn = document.querySelector(".reset").addEventListener("click", resetProgram);
+
+// size buttons
+const smallbtn = document.querySelector(".small").addEventListener("click", () => {makeGrid(6)});
+const mediumbtn = document.querySelector(".medium").addEventListener("click", () => {makeGrid(9)});
+const largebtn = document.querySelector(".large").addEventListener("click", () => {makeGrid(12)});

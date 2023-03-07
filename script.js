cdee238e564bda9c4590f3113a78725df363d807
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-03-07 14:07:47 +0800

    changes

--- script.js
+++ script.js
@@ -2,23 +2,31 @@
 const pad = document.querySelector(".container");
 const startbtn = document.createElement("button");
 
-// buttons
-const xsmallbtn = document.querySelector(".xsmall").addEventListener("click", function(){makeGrid(3);});
+// size buttons
+const xsmallbtn = document.querySelector(".xsmall").addEventListener("click", function(){makeGrid(4,4);});
 const smallbtn = document.querySelector(".small");
 const mediumbtn = document.querySelector(".medium");
 const largebtn = document.querySelector(".large");
 const xlargebtn = document.querySelector(".xlarge");
 
+// tools buttons
+const rainbowbtn = document.querySelector(".rainbowmode");
+const drawbtn = document.querySelector(".drawbtn");
+const eraserbtn = document.querySelector(".eraser");
+const resetbtn = document.querySelector(".reset");
+
 
 
 
 // to make squares inside the grid
-function makeGrid(number) {
-    let size = number*number;
+function makeGrid(rows, cols) {
+    pad.style.setProperty('--grid-rows', rows);
+    pad.style.setProperty('--grid-cols', cols);
+    let size = rows*cols;
     for (let i = 0; i < size; i ++) {
         let square = document.createElement("div");
-        pad.appendChild(square);
         square.style.cssText = "border: 1px solid grey; border-radius: 10px;"
+        pad.appendChild(square).className = "grid-items";
     }
 }
 

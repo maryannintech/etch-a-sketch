191a481dff38ba37d79703216b18e0067937ec7c
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-03-08 14:00:30 +0800

    made a drawingtools function

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-// to make divs inside the container
 function makeGrid(num) {
     const pad = document.querySelector(".container");
-    bottomtexthide = document.querySelector(".bottomtext");
-    sizesbtns = document.querySelector(".sizes");
+    const bottomtexthide = document.querySelector(".bottomtext");
+    const sizesbtns = document.querySelector(".sizes");
 
     const size = num **2;
     // to make use of all the columns and rows
     pad.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     pad.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+    // loop for adding divs
     for (let i = 0; i < size; i ++) {
         const square = document.createElement("div");
         square.style.cssText = "border: 1px solid grey; border-radius: 5px;"
         pad.appendChild(square).className = "grid-items";
     }
 
+    // to hide size buttons and text after the user chose
     bottomtexthide.style.visibility = "hidden";
     sizesbtns.style.visibility = "hidden";
 
@@ -26,15 +27,17 @@ function makeGrid(num) {
     })
 }
 
-// generate random color by given pallete
-function chooseRandomColor() {
-    let pallete = ['#386641', '#6A994E', '#A7C957', '#F2E8CF', '#BC4749'];
-    let randomcolorChoice = pallete[Math.floor(Math.random()*pallete.length)];
-}
 
 // to let user draw 
-function draw() {
-
+function drawingTools(tools) {
+    if (tools === 'random') {
+        let pallete = ['#386641', '#6A994E', '#A7C957', '#F2E8CF', '#BC4749'];
+        let randomColor = pallete[Math.floor(Math.random()*pallete.length)];
+        //e.target.style.backgroundColor = `rgb(${randomColor}, ${randomColor}, ${randomColor})`;
+    }
+    else if (tools === 'color') {
+        
+    }
 }
 
 function erase() {
@@ -46,12 +49,13 @@ function resetProgram() {
 }
 
 // tools buttons
-const rainbowbtn = document.querySelector(".randommode");
-const drawbtn = document.querySelector(".drawbtn");
-const eraserbtn = document.querySelector(".eraser");
+const randomColor = document.querySelector(".randommode").addEventListener("click", drawingTools("random"));
+const drawbtn = document.querySelector(".draw").addEventListener("click", drawingTools("color"));
+const eraserbtn = document.querySelector(".eraser").addEventListener("click", erase);
 const resetbtn = document.querySelector(".reset").addEventListener("click", resetProgram);
 
 // size buttons
 const smallbtn = document.querySelector(".small").addEventListener("click", () => {makeGrid(6)});
 const mediumbtn = document.querySelector(".medium").addEventListener("click", () => {makeGrid(9)});
 const largebtn = document.querySelector(".large").addEventListener("click", () => {makeGrid(12)});
+
